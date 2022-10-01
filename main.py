ffa6423652c11fedcd086f5cd436df71f61c7dd3
student <tarasovych.maria@gmail.com>
2022-10-01 10:34:22 +0300

    5 ex

---
+++ main.py
@@ -0,0 +1,13 @@
+number_1 = int(input('1 number -> '))
+number_2 = int(input('2 number -> '))
+
+if number_1%2 == 0:
+    number_1+=1
+
+while number_2 <= number_1:
+    print(f'number = {number_2}')
+    number_2 += 2
+
+while number_1 <= number_2:
+    print(f'number = {number_1}')
+    number_1 += 2
